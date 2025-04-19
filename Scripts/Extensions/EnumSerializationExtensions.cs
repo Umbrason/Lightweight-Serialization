5b8924df563728c76321490a4f1652c29683133f
Umbrason <Umbrason@Outlook.com>
2025-04-19 22:26:56 +0200

    Update EnumSerializationExtensions.cs

--- Scripts/Extensions/EnumSerializationExtensions.cs
+++ Scripts/Extensions/EnumSerializationExtensions.cs
@@ -1,8 +1,8 @@
 public static class EnumSerializationExtensions
 {
-    public static void WriteEnum<T>(this ISerializer serializer, string name, T value) where T : Enum
+    public static void WriteEnum<T>(this ISerializer serializer, string name, T value) where T : System.Enum
         => serializer.WriteInt(name, (int)value);
 
-    public static T ReadEnum<T>(this IDeserializer deserializer, string name) where T : Enum
+    public static T ReadEnum<T>(this IDeserializer deserializer, string name) where T : System.Enum
         => (T)deserializer.ReadInt(name);
 }
